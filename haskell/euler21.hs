ba0e02a6b0cf9b59a0dbeaf523133d75cbac9ba9
BerkshireKnight <ian.knight.1990@gmail.com>
2013-08-30 16:23:30 +0100

    [21] Solved problem 21 in Haskell - applied idiot filter, and removed unnecessary comparisons in amic_list - deleted incomplete python solution

--- haskell/euler21.hs
+++ haskell/euler21.hs
@@ -1,11 +1,14 @@
 factors         ::  Int -> [Int]
 factors n       =   [m | m <- [1..n-1], n `mod` m == 0]
 
+factor_sum      ::  Int -> Int
+factor_sum n    =   sum . factors $ n
+
 amicable        ::  (Int,Int) -> Bool
-amicable (n,m)  =   (sum (factors n) == m) && (sum (factors m) == n)
+amicable (n,m)  =   factor_sum n == m && factor_sum m == n
 
 amic_list       ::  Int -> [(Int,Int)]
-amic_list n     =   [(x,y) | x <- [1..n], y <- [x+1..n], amicable (x,y)]
+amic_list n     =   [(x,factor_sum x) | x <- [1..n], amicable (x,factor_sum x), x < factor_sum x]
 
 euler21         ::  Int
 euler21         =   sum [x+y | (x,y) <- amic_list 9999]
