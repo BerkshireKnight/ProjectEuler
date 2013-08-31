ceb2cd30653a676ab6206d2ab9d0fa7a0ccc0b94
BerkshireKnight <ian.knight.1990@gmail.com>
2013-08-31 11:19:15 +0100

    Improved factor-finding algorithms - only check digits up to n/2, and then calculate its opposite number - defined pfactors and ntfactors in terms of factors

--- haskell/Factors.hs
+++ haskell/Factors.hs
@@ -1,12 +1,15 @@
-module Factors where
+module Factors (factors, pfactors, ntfactors) where
+    import Data.List (sort, nub)
+
     -- returns a list of the factors of n
     factors         ::  Int -> [Int]
-    factors n       =   [m | m <- [1..n], n `mod` m == 0]
+    factors n       =   sort . nub $ fs where
+                            fs = foldr (++) [] [[m,n `div` m] | m <- [1..(n`div`2)+1], n `mod` m == 0]
 
     -- returns a list of the proper factors of n
     pfactors        ::  Int -> [Int]
-    pfactors n      =   [m | m <- [1..n-1], n `mod` m == 0]
+    pfactors n      =   init . factors $ n
 
     -- returns a list of the non-trivial factors of n
     ntfactors       ::  Int -> [Int]
-    ntfactors  n    =   [m | m <- [2..n-1], n `mod` m == 0]
+    ntfactors  n    =   tail . pfactors $ n
