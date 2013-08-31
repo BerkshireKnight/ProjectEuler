806340681a61d97a2095e9289feeb07f531302f3
BerkshireKnight <ian.knight.1990@gmail.com>
2013-08-31 12:53:56 +0100

    Various small changes to Haskell solutions - added main methods for compilation - extended gitignore

--- haskell/euler12.hs
+++ haskell/euler12.hs
@@ -1,17 +1,17 @@
-import Factors (factors)
+module Main where
+    import Factors (factors)
 
--- returns the number of factors (divisors) of n
-num_factors     ::  Int -> Int
-num_factors n   =   length . factors $ n
+    -- returns the number of factors (divisors) of n
+    num_factors     ::  Int -> Int
+    num_factors n   =   length . factors $ n
 
--- returns the nth triangle number
-triangle        ::  Int -> Int
-triangle n      =   n * (n+1) `div` 2
+    -- returns the nth triangle number
+    triangle        ::  Int -> Int
+    triangle n      =   n * (n+1) `div` 2
 
--- finds the first triangle number with over 500 factors (divisors)
-euler12                         ::  Int -> Int
-euler12 n | num_factors n > 500 =   n
-          | otherwise           =   euler12 . triangle $ n+1
+    -- finds the first triangle number with over 500 factors (divisors)
+    euler12                         ::  Int -> Int
+    euler12 n | num_factors n > 500 =   n
+              | otherwise           =   euler12 . triangle $ n+1
 
-solution        ::  Int
-solution        =   euler12 1
+    main = print . euler12 $ 1
--- haskell/euler21.hs
+++ haskell/euler21.hs
@@ -1,13 +1,13 @@
-import Factors (pfactors)
+module Main where
+    import Factors (pfactors)
 
-factor_sum      ::  Int -> Int
-factor_sum n    =   sum . pfactors $ n
+    factor_sum      ::  Int -> Int
+    factor_sum n    =   sum . pfactors $ n
 
-amicable        ::  (Int,Int) -> Bool
-amicable (n,m)  =   factor_sum n == m && factor_sum m == n
+    amicable        ::  (Int,Int) -> Bool
+    amicable (n,m)  =   factor_sum n == m && factor_sum m == n
 
-amic_list       ::  Int -> [(Int,Int)]
-amic_list n     =   [(x,factor_sum x) | x <- [1..n], amicable (x,factor_sum x), x < factor_sum x]
+    amic_list       ::  Int -> [(Int,Int)]
+    amic_list n     =   [(x,factor_sum x) | x <- [1..n], amicable (x,factor_sum x), x < factor_sum x]
 
-euler21         ::  Int
-euler21         =   sum [x+y | (x,y) <- amic_list 9999]
+    main            =   print . sum $ [x+y | (x,y) <- amic_list 9999]
