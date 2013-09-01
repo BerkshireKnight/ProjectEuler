a3461923044c7cdb5485aa92ba04d6f1599840ad
BerkshireKnight <ian.knight.1990@gmail.com>
2013-09-01 13:43:21 +0100

    [27] Started work on problem 27 - created a Primes library in Haskell

---
+++ haskell/euler27.hs
@@ -0,0 +1,14 @@
+module Main where
+    import Primes (prime)
+
+    -- resolves the quadratic formula n^2 + an + b
+    quadratic           ::  Int -> Int -> Int -> Int
+    quadratic n a b     =   n*n + a*n + b
+
+    -- counts the number of consecutive prime numbers obtained from resolving
+    -- the quadratic formula with the given values of n, a, and b
+    consec_primes       ::  Int -> Int -> Int -> Int
+    consec_primes n a b =   if prime $ quadratic (n+1) a b
+                                then consec_primes (n+1) a b
+                                else n+1
+
