dfad71aebcc1fd6b5acf209b5f018c9e1cd515e4
BerkshireKnight <ian.knight.1990@gmail.com>
2013-09-04 16:48:48 +0100

    [27] Solved problem 27

--- haskell/euler27.hs
+++ haskell/euler27.hs
@@ -15,7 +15,7 @@ module Main where
     -- returns a list of pairs of values for a and b, paired with the number of
     -- consecutive primes produced by those pairs
     consec_list         ::  Int -> Int -> [((Int,Int),Int)]
-    consec_list min max =   [((a,b),consec_primes 0 a b) | a <- [min..max], b <- [min..max], consec_primes 0 a b > 1]
+    consec_list min max =   [((a,b),consec_primes 0 a b) | a <- [min..max], b <- [min..max], consec_primes 0 a b > 40]
 
     -- sorts the results of consec_list accordng to the number of consecutive primes produced
     qsort               ::  [((Int,Int),Int)] -> [((Int,Int),Int)]
@@ -24,14 +24,16 @@ module Main where
                                 ys  =   [y | y <- xs, snd y <= snd x]
                                 zs  =   [z | z <- xs, snd z > snd x]
 
-    result              ::  ((Int,Int),Int)
-    result              =   last $ qsort $ consec_list (-999) 999
+    -- obtains the pair of coefficients between -999 and 999 which produce the greatest number
+    -- of consecutive primes
+    solution            ::  ((Int,Int),Int)
+    solution            =   last $ qsort $ consec_list (-999) 999
 
-    main                =   do  let coefficients = fst result
+    main                =   do  let coefficients = fst solution
                                     a = fst coefficients
                                     b = snd coefficients
                                     p = a * b
-                                    x = snd result
+                                    x = snd solution
                                 putStrLn ("The coefficients are a=" ++ show a ++ ", b=" ++ show b)
                                 putStrLn ("They produce " ++ show x ++ " consecutive primes")
                                 putStrLn ("Their product is " ++ show p)
