b211d3fa1aa59bb6c0ec908c3c997ef764b6b25a
BerkshireKnight <ian.knight.1990@gmail.com>
2013-09-04 10:49:07 +0100

    [28] Tidied code for solution 28

--- haskell/euler28.hs
+++ haskell/euler28.hs
@@ -1,16 +1,21 @@
 module Main where
 
+    -- returns the values of the four corners of the square that is r iterations out
+    -- from the centre
     cornerNumbers           ::  Int -> (Int,Int,Int,Int)
     cornerNumbers r          =   (n, n - f', n - 2*f', n - 3*f') where
                                     f   = 2*r + 1
                                     n   = f*f
                                     f'  = f - 1
 
-    quadrupleSum            ::  (Int,Int,Int,Int) -> Int
-    quadrupleSum (a,b,c,d)  =   a + b + c + d
-
+    -- returns four-tuples containing the values of all corner numbers in all squares
+    -- at most r iterations out from the centre
     cornersToWidth          ::  Int -> [(Int,Int,Int,Int)]
     cornersToWidth w        =   [cornerNumbers r | r <- [1..w]]
 
+    -- sums the values of a four-tuple of integers
+    quadrupleSum            ::  (Int,Int,Int,Int) -> Int
+    quadrupleSum (a,b,c,d)  =   a + b + c + d
 
-    main                    =   do  putStrLn (show $ sum [quadrupleSum cs | cs <- cornersToWidth 500] + 1)
+
+    main                    =   putStrLn (show $ sum [quadrupleSum cs | cs <- cornersToWidth 500] + 1)
