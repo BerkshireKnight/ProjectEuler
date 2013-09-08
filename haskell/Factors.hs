a6e7f6fd8b62181890930f98c797b7f1a83a6c11
BerkshireKnight <ian.knight.1990@gmail.com>
2013-09-08 14:54:36 +0100

    Refined Factors module - exchanged Int type for Integral typeclass - redefined pfactors and ntfactors for clarity - fixed guards on prime to ensure that `prime 1` returns False without having to calculate

--- haskell/Factors.hs
+++ haskell/Factors.hs
@@ -2,20 +2,20 @@ module Factors (factors, pfactors, ntfactors, prime) where
     import Data.List (sort, nub)
 
     -- returns a list of the factors of n
-    factors             ::  Int -> [Int]
+    factors             ::  Integral a => a -> [a]
     factors n           =   sort . nub $ fs where
                                 fs  =   concat [[m,n `div` m] | m <- [1..lim+1], n `mod` m == 0]
                                 lim =   floor . sqrt . fromIntegral $ n
 
     -- returns a list of the proper factors of n
-    pfactors            ::  Int -> [Int]
-    pfactors n          =   init . factors $ n
+    pfactors            ::  Integral a => a -> [a]
+    pfactors            =   init . factors
 
     -- returns a list of the non-trivial factors of n
-    ntfactors           ::  Int -> [Int]
-    ntfactors  n        =   tail . pfactors $ n
+    ntfactors           ::  Integral a => a -> [a]
+    ntfactors           =   tail . pfactors
 
     -- returns True if n is prime, False if not
-    prime               ::  Int -> Bool
-    prime n | n <= 0    =   False
-            | otherwise =   (length . factors $ n) == 2
+    prime               ::  Integral a => a -> Bool
+    prime n | n <= 1    =   False
+            | otherwise =   factors n == [1,n]
