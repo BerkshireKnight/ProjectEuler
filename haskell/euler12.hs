e2e021a6f8829b6df8d57f7df3a52876ef48e23f
BerkshireKnight <ian.knight.1990@gmail.com>
2013-10-04 08:49:47 +0100

    Minor changes

--- haskell/euler12.hs
+++ haskell/euler12.hs
@@ -3,15 +3,23 @@ module Main where
 
     -- returns the number of factors (divisors) of n
     num_factors     ::  Int -> Int
-    num_factors n   =   length . factors $ n
+    num_factors     =   length . factors
 
     -- returns the nth triangle number
     triangle        ::  Int -> Int
     triangle n      =   n * (n+1) `div` 2
 
+    triangles       ::  [(Int,Int,Int)]
+    triangles       =   [(n, triangle n, num_factors . triangle $ n) | n <- [1..100]]
+
+    print_triangles         ::  [(Int,Int,Int)] -> IO ()
+    print_triangles [t]     =   putStrLn . show $ t
+    print_triangles (t:ts)  =   do  putStrLn . show $ t
+                                    print_triangles ts
+
     -- finds the first triangle number with over 500 factors (divisors)
     euler12                         ::  Int -> Int
     euler12 n | num_factors n > 500 =   n
-              | otherwise           =   euler12 . triangle $ n+1
+              | otherwise           =   euler12 . triangle $ (n+1)
 
     main = print . euler12 $ 1
--- haskell/euler15.hs
+++ haskell/euler15.hs
@@ -1,4 +1,6 @@
 module Main where
+    import System.IO (hFlush, stdout)
+
     data Coords                 =   Coords Int Int deriving Show
     data PathTree               =   EndPoint | Branch PathTree PathTree deriving Show
 
@@ -9,16 +11,18 @@ module Main where
                                         then EndPoint
                                         else Branch (buildPaths n (Coords x (y+1))) (buildPaths n (Coords (x+1) y))
 
-
     countPaths                  ::  PathTree -> Int
     countPaths (EndPoint)       =   1
     countPaths (Branch p1 p2)   =   countPaths p1 + countPaths p2
 
-
     origin                      ::  Coords
     origin                      =   Coords 0 0
 
+    getGridSize                 ::  IO String
+    getGridSize                 =   do  putStr "Enter the size of the grid: "
+                                        hFlush stdout
+                                        getLine
 
-    main                        =   do  let paths = buildPaths 20 origin
-                                            ans = countPaths paths
+    main                        =   do  n <- getGridSize
+                                        let ans = countPaths $ buildPaths (read n) origin
                                         putStrLn $ "There are " ++ show ans ++ " paths through that grid."
