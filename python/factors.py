0d8d20cf7107f6877b357637bc8e534a4a38636e
BerkshireKnight <ian.knight.1990@gmail.com>
2016-08-01 10:08:30 +0100

    Adds factors.py module

---
+++ python/factors.py
@@ -0,0 +1,32 @@
+
+from math import sqrt
+
+ALL = 0
+PROPER = 1
+NON_TRIVIAL = 2
+
+
+def find_factors(x, t):
+    results = set()
+    for x1 in range(1, sqrt(x)+2):
+        if t == NON_TRIVIAL and x1 == 1:
+            continue
+        elif t == PROPER and x1 == 1:
+            results.add(x1)
+        else:
+            results.add(x1)
+            results.add(x // x1)
+
+    return results
+
+
+def factors(x):
+    return find_factors(x, ALL)
+
+
+def proper_factors(x):
+    return find_factors(x, PROPER)
+
+
+def non_trivial_factors(x):
+    return find_factors(x, NON_TRIVIAL)
