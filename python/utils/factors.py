e801d00c7d84864cc64d3f2d691716d863a8a74f
BerkshireKnight <ian.knight.1990@gmail.com>
2016-08-11 12:52:09 +0100

    Fixes bugs in factors

--- python/utils/factors.py
+++ python/utils/factors.py
@@ -1,35 +1,23 @@
 
 from math import sqrt
 
-ALL = 0
-PROPER = 1
-NON_TRIVIAL = 2
 
-
-def find_factors(x, t):
+def factors(x):
     results = set()
-    for x1 in range(1, sqrt(x)+2):
-        if t == NON_TRIVIAL and x1 == 1:
-            continue
-        elif t == PROPER and x1 == 1:
-            results.add(x1)
-        else:
+    for x1 in range(1, int(sqrt(x)+2)):
+        if x % x1 == 0:
             results.add(x1)
             results.add(x // x1)
 
-    return results
-
-
-def factors(x):
-    return find_factors(x, ALL)
+    return sorted(results)
 
 
 def proper_factors(x):
-    return find_factors(x, PROPER)
+    return factors(x)[1:]
 
 
 def non_trivial_factors(x):
-    return find_factors(x, NON_TRIVIAL)
+    return factors(x)[1:-1]
 
 
 def is_prime(x):
