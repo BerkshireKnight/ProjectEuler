9cdda6877c6f8e87f0a6b9d8c6c3beaa710f02a1
BerkshireKnight <ian.knight.1990@gmail.com>
2017-06-18 17:41:07 +0100

    Completes problem 12

--- python/euler12.py
+++ python/euler12.py
@@ -1,12 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+from functools import reduce
+from itertools import chain
 from math import sqrt
 
 
 def factors(n):
-    return set(reduce(list.__add__,
-                ([i, n//i] for i in range(1, int(sqrt(n)) + 1) if n % i == 0))
-    )
+    factor_pairs = [(k, n//k) for k in range(1, int(sqrt(n))+1) if n % k == 0]
+    factors = list(chain(*factor_pairs))
+    return sorted(factors)
 
 
 def triangle(n):
@@ -14,16 +16,16 @@ def triangle(n):
 
 
 def solution(n):
-    val = n
-    while len(factors(triangle(val))) <= 500:
-        n += 1
+    k = 1
+    while True:
+        if len(factors(triangle(k))) >= n:
+            return triangle(k)
 
-    return triangle(val)
-
-
-def main():
-    print solution(1)
+        k += 1
 
 
 if __name__ == "__main__":
-    main()
+    import sys
+
+    n = int(sys.argv[1])
+    print(solution(n))
