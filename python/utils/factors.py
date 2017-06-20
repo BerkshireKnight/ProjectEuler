33c29b59b0da7aa3c79c0161daec1482478d81a5
BerkshireKnight <ian.knight.1990@gmail.com>
2017-06-20 13:52:20 +0100

    Makes sorting of factors optional

--- python/utils/factors.py
+++ python/utils/factors.py
@@ -2,22 +2,22 @@
 from math import sqrt
 
 
-def factors(x):
+def factors(x, sorted=False):
     results = []
     for x1 in range(1, int(sqrt(x)+2)):
         if x % x1 == 0:
             results.append(x1)
             results.append(x // x1)
 
-    return sorted(results)
+    return sorted(results) if sorted else results
 
 
 def proper_factors(x):
-    return factors(x)[1:]
+    return factors(x, True)[1:]
 
 
 def non_trivial_factors(x):
-    return factors(x)[1:-1]
+    return factors(x, True)[1:-1]
 
 
 def is_prime(x):
