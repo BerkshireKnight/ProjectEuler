ffedb248ee362903272c0ec70c3b214b67b6d1be
BerkshireKnight <ian.knight.1990@gmail.com>
2017-06-20 14:07:56 +0100

    Progress on problem 23

--- python/euler23.py
+++ python/euler23.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python3
 
-from utils.factors import factors
+from utils.factors import proper_factors
 
 
-def abundant(n):
-    return sum(factors(n)) > n
+def get_abundant(n):
+
+    def abundant(m):
+        return sum(proper_factors(m)) > m
+
+    return [n for n in range(n) if abundant(n)]
 
 
 def euler23():
-    ns = [n for n in range(12, 28124) if abundant(n)]
+    ns, ms = get_abundant(28124), set()
 
     i, j = 0, 0
-    sums = set()
     while i < len(ns):
         while j < len(ns):
-            sums.add(ns[i] + ns[j])
+            ms.add(ns[i] + ns[j])
             j += 1
 
         i += 1
         j = i
+
+    total = 0
+    for k in range(28124):
+        if k not in ms:
+            total += k
+
+    return k
+
+
+if __name__ == '__main__':
+    print(euler23())
--- python/utils/factors.py
+++ python/utils/factors.py
@@ -2,18 +2,19 @@
 from math import sqrt
 
 
-def factors(x, sorted=False):
-    results = []
-    for x1 in range(1, int(sqrt(x)+2)):
+def factors(x, sort=False):
+    results = set()
+    for x1 in range(1, int(sqrt(x)+1)):
         if x % x1 == 0:
-            results.append(x1)
-            results.append(x // x1)
+            results.add(x1)
+            results.add(x // x1)
 
-    return sorted(results) if sorted else results
+    results = list(results)
+    return sorted(results) if sort else results
 
 
 def proper_factors(x):
-    return factors(x, True)[1:]
+    return factors(x, True)[:-1]
 
 
 def non_trivial_factors(x):
